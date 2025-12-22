e156cd63e7368524647dfd5d7125718d97b3d441
Kieran Schwegman <kschwegman1@gmail.com>
2025-12-21 19:21:42 -0500

    adding the class structure for the project

--- Libraries/Classes/src/index.ts
+++ Libraries/Classes/src/index.ts
@@ -1,1 +1,15 @@
-export * from './lib/classes.js';
+export * from './Models/classes.ts';
+
+// Frontend
+export * from './Frontend/TokenInformation.ts';
+
+// Models
+export * from './Models/Policies/PolicySubmission.ts';
+export * from './Models/Policies/Policy.ts';
+export * from './Models/Policies/AutoPolicy.ts';
+export * from './Models/Policies/HomePolicy.ts';
+
+export * from './Models/Clients/Client.ts';
+export * from './Models/Agents/Agent.ts';
+
+// Common
