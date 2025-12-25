928bfbc8a2138e08bc0a4443e83cb222e39aab4b
Kieran Schwegman <kschwegman1@gmail.com>
2025-12-25 11:23:27 -0500

    scaffolding for the select component

---
+++ libraries/ReactComponents/src/Forms/Select/SelectItem/SelectItem.tsx
@@ -0,0 +1,11 @@
+import styles from './SelectItem.module.scss';
+
+export function SelectItem() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to SelectItem!</h1>
+    </div>
+  );
+}
+
+export default SelectItem;
