e3fae006ced57c9c012a24df690b7736c08f656e
Kieran Schwegman <kschwegman1@gmail.com>
2026-04-25 18:57:37 -0400

    fixed a circular dependency error

--- libraries/ReactComponents/src/Common/Utilities/Tooltip/Tooltip.tsx
+++ libraries/ReactComponents/src/Common/Utilities/Tooltip/Tooltip.tsx
@@ -1,9 +1,9 @@
 import { ReactNode, RefObject, Suspense, useEffect, useMemo, useRef, useState } from 'react';
 import { CodeRenderer } from '../CodeSnippets/CodeRenderer';
-import { Icon, interpFloat, interpV2 } from '@Project/ReactComponents'; // todo: this needs to be fixed
 
 import styled from '@emotion/styled';
 import styles from './Tooltip.module.scss';
+import { Icon, interpFloat, interpV2 } from '../..';
 
 
 // Tooltip variants 
