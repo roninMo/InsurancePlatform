36fc413f5be6c665854375c9dbd77455fc9a3783
Kieran Schwegman <kschwegman1@gmail.com>
2026-06-06 14:24:30 -0400

    moving the notifications component to the react components library

--- libraries/ReactComponents/src/Common/Content/index.ts
+++ libraries/ReactComponents/src/Common/Content/index.ts
@@ -4,6 +4,7 @@
 export * from './Alert/Alert';
 export * from './Card/Card';
 export * from './Dropdown/Dropdown';
+export * from './Notification/Notification';
 
 // Height Transition Wrapper
 export * from './HeightTransWrapper/HeightTransWrapper';
